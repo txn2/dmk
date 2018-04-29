407c29e535de04a3a0df9bdd1bc7172cc6cdf5f9
Craig Johnston <cjimti@gmail.com>
2018-04-28 23:43:33 -0700

    gui and logout flags

--- cli/run.go
+++ cli/run.go
@@ -5,8 +5,12 @@ import (
 
 	"log"
 
+	"io/ioutil"
+
 	"github.com/desertbit/grumble"
+	"github.com/gizak/termui"
 	"github.com/txn2/dmk/migrate"
+	"github.com/txn2/dmk/panel"
 )
 
 func init() {
@@ -19,6 +23,7 @@ func init() {
 		Flags: func(f *grumble.Flags) {
 			f.Bool("d", "dry-run", false, "Dry run outputs the first 5 records.")
 			f.Bool("v", "verbose", false, "Verbose output.")
+			f.Bool("l", "logout", false, "Log style output, no GUI.")
 		},
 		Run: func(c *grumble.Context) error {
 			if ok := activeProjectCheck(); ok {
@@ -40,6 +45,7 @@ func init() {
 
 }
 
+// runMigration runs a migration
 func runMigration(machineName string, f grumble.FlagMap, args []string) {
 
 	runnerCfg := migrate.RunnerCfg{
@@ -53,21 +59,88 @@ func runMigration(machineName string, f grumble.FlagMap, args []string) {
 
 	runner := migrate.NewRunner(runnerCfg)
 
-	// todo: display stats when the result becomes useful
 	res, err := runner.RunAsync(machineName, args)
 	if err != nil {
 		Cli.PrintError(err)
 	}
 
+	if f.Bool("logout") {
+		runMigrationLogOut(res)
+		return
+	}
+
+	runMigrationGui(res)
+}
+
+// runMigrationLogOut plain log output if requested by
+// the -l flag.
+func runMigrationLogOut(res *migrate.RunResult) {
 	for {
 		select {
 		case <-res.Done:
+			log.Printf("Done.")
 			return
 		case msg := <-res.Status: // TODO log output?
-			log.Printf("%s", msg)
+			log.Printf(msg)
 		case err := <-res.Error:
 			fmt.Printf("Got error: %s", err.Error())
+			return
 		}
 	}
+}
 
+// runMigrationGui uses termui to display migration status
+func runMigrationGui(res *migrate.RunResult) {
+	err := termui.Init()
+
+	if err != nil {
+		panic(err)
+	}
+	defer termui.Close()
+
+	// handle key q pressing
+	termui.Handle("/sys/kbd/q", func(termui.Event) {
+		// press q to quit
+		termui.StopLoop()
+	})
+
+	ins := termui.NewPar("Press [q] to quit at any time.")
+	ins.Height = 1
+	ins.Border = false
+
+	status := panel.NewLogPanel("Migration Status", 8)
+
+	termui.Body.AddRows(
+		termui.NewRow(
+			termui.NewCol(12, 0, status.Panel),
+		),
+		termui.NewRow(
+			termui.NewCol(12, 0, ins),
+		),
+	)
+
+	termui.Body.Align()
+
+	termui.Render(ins)
+
+	// use the -l (logout) flag if script output is needed.
+	log.SetFlags(0)
+	log.SetOutput(ioutil.Discard)
+
+	go func() {
+		for {
+			select {
+			case <-res.Done:
+				status.AddMessage(fmt.Sprintf("Done."))
+				return
+			case msg := <-res.Status:
+				status.AddMessage(msg)
+			case err := <-res.Error:
+				status.AddMessage(fmt.Sprintf("Got error: %s", err.Error()))
+				return
+			}
+		}
+	}()
+
+	termui.Loop()
 }
---
+++ panel/panel.go
@@ -0,0 +1,42 @@
+package panel
+
+import "github.com/gizak/termui"
+
+// logPanel extends a termui List widget
+// see NewLogPanel
+type logPanel struct {
+	Panel    *termui.List
+	messages []string
+}
+
+// NewLogPanel produces a termui widget for rendering rolling logs
+// for use in grid layouts
+func NewLogPanel(title string, height int) *logPanel {
+	ls := termui.NewList()
+	ls.ItemFgColor = termui.ColorYellow
+	ls.BorderLabel = title
+	ls.Height = height
+
+	termui.Render(ls)
+
+	return &logPanel{
+		Panel:    ls,
+		messages: make([]string, 0),
+	}
+}
+
+// AddMessage adds a message to the list
+func (lp *logPanel) AddMessage(msg string) {
+	lp.messages = append(lp.messages, msg)
+
+	maxRows := lp.Panel.Height - 2
+
+	if len(lp.messages) > maxRows {
+		lp.Panel.Items = lp.messages[(len(lp.messages) - maxRows):]
+		termui.Render(lp.Panel)
+		return
+	}
+
+	lp.Panel.Items = lp.messages
+	termui.Render(lp.Panel)
+}
